97680949b73fa75ed6b579716e67fd98637f9080
Aravind Pedapudi <aravindp1510@gmail.com>
2013-08-17 18:05:33 +0530

    Brick animation

--- Board.java
+++ Board.java
@@ -21,8 +21,8 @@ public class Board extends JPanel implements ActionListener{
 	private Timer timer2;
 	private Ball ball;
 	private double angle, angleInc;
-	private int screenX, screenY, xinc, wallx;
-	private final double ANGLE_INC_ACCEL, ANGLE_INC;
+	private int screenX, screenY, wallx;
+	private final double TIME_ACCEL, TIME_NORM;
 	private ArrayList<Wall> walls;
 	public Board(int screenW, int screenH){
 		addKeyListener(new TAdapter());
@@ -33,14 +33,13 @@ public class Board extends JPanel implements ActionListener{
 		screenY = screenH;		
 		ball = new Ball();
 		angle = 0;
-		angleInc = 0.02;
-		xinc = 5;
 		wallx = 0;
 		timer = new Timer(5, this);
 		timer.start();
 		timer2 = null;
-		ANGLE_INC_ACCEL = 0.08;
-		ANGLE_INC = 0.02;
+		TIME_NORM = 0.02;
+		TIME_ACCEL = 0.08;
+		angleInc = TIME_NORM;
 		
 		int x = 0;
 		walls = new ArrayList<Wall>();
@@ -101,7 +100,6 @@ public class Board extends JPanel implements ActionListener{
 	
 	public void paint(Graphics g){
 		super.paint(g);
-		int i;
 		Graphics2D g2d = (Graphics2D) g;
 		AffineTransform at = AffineTransform.getTranslateInstance(100, screenY-100+ball.getY());
 		at.rotate(angle, ball.getImage().getWidth(this)*0.5, ball.getImage().getHeight(this)*0.5);
@@ -110,11 +108,17 @@ public class Board extends JPanel implements ActionListener{
 			angle = 0;
 		g2d.drawImage(ball.getImage(), at, this);
 		
-		for(Wall wall : walls){
-			g2d.drawImage(wall.getImage(), (wall.getX() + wallx) % screenX, wall.getY() + ball.getImage().getHeight(this), this);
-		}
+		if(wallx < screenX)
+			for(Wall wall : walls){
+				g2d.drawImage(wall.getImage(), (wall.getX() + wallx) % screenX, wall.getY() + ball.getImage().getHeight(this), this);
+			} //Base image
 		
-		wallx -= angleInc * 50;
+		for(Wall wall : walls){
+			g2d.drawImage(wall.getImage(), screenX + (wall.getX() + wallx) % screenX - wall.getImgW(), wall.getY() + ball.getImage().getHeight(this), this);
+		} // Looping image
+		
+		wallx -= (angleInc * 50)%screenX; //50 factor to convert the time scale into integer pixels
+		
 		
 		Toolkit.getDefaultToolkit().sync();
 		g.dispose();
@@ -141,22 +145,22 @@ public class Board extends JPanel implements ActionListener{
 	public class TAdapter extends KeyAdapter{
 		public void keyReleased(KeyEvent e){
 			int keycode = e.getKeyCode();
-			if(keycode == KeyEvent.VK_UP){
-				startTimer2();
-				ball.animate();
-				
-			}
 			if(keycode == KeyEvent.VK_RIGHT)
-				angleInc = ANGLE_INC;
+				angleInc = TIME_NORM;
 			if(keycode == KeyEvent.VK_LEFT)
-				angleInc = ANGLE_INC;
+				angleInc = TIME_NORM;
 		}
 		public void keyPressed(KeyEvent e){
 			int keycode = e.getKeyCode();
+			if(keycode == KeyEvent.VK_UP){
+				startTimer2();
+				ball.animate();
+				//better responsiveness with this block in keyPressed than in keyReleased
+			}
 			if(keycode == KeyEvent.VK_RIGHT)
-				angleInc = ANGLE_INC_ACCEL;
+				angleInc = TIME_ACCEL;
 			if(keycode == KeyEvent.VK_LEFT)
-				angleInc = - ANGLE_INC_ACCEL;
+				angleInc = - TIME_ACCEL;
 		}
 	}
 }
